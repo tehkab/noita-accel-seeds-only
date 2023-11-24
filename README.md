5b015b185be48a472afdad5512e848a9a848b582
Kabby <1925023+tehkab@users.noreply.github.com>
2023-11-24 15:11:58 -0500

    TEST CODE for generating seed list

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Noita Acceleratium Starts Only
+## TEST CODE
+The 'test-seed-list' branch will generate a file containing all acceleratium start seed numbers.
+This test will run at the start of a run: The game will appear to hang and you will almost certainly get the  "Well, this is embarrassing" screen.
+Do not panic! the process is still running in the background. When it is done, the game will finish loading and start the run as normal (on seed 2147483647)
 
-This mod forces the game seed to be one that starts you with an Acceleratium flask.
-It will search for true acceleratium starts only, instead of simply changing the flask.
-This means the starts will be valid "vanilla" starts and therefore suitable for speedrunning.
-
+The output will appear in the mod folder ("mods/accel-seeds-only/test-seed-list")
--- init.lua
+++ init.lua
@@ -1,18 +1,32 @@
+dofile_once("data/scripts/lib/coroutines.lua")
+dofile_once( "data/scripts/lib/utilities.lua")
 dofile_once("data/scripts/items/potion_starting.lua")
 
-function OnMagicNumbersAndWorldSeedInitialized()
-    local new_seed = tonumber(StatsGetValue("world_seed"))
-    for tries=0,1000 do --should be plenty of tries; but want to 'time out' if something goes wrong too
+function OnModPostInit()
+    --this doesnt actually run async(?), not sure why
+    --but it generates the output okay nevertheless
+    --at the cost of appearing to hang (with nolla's fun error screen) while loading :)
+    async(test_all_seeds())
+end
+
+function test_all_seeds()
+    local f = io.open("mods/accel-seeds-only/test-seed-list","w")
+    local frame = GameGetFrameNum()
+    io.output(f)
+    for new_seed=1,0x7FFFFFFF do
         SetWorldSeed(new_seed)
         --i dont exactly know *why* these constants but they give the correct results
         SetRandomSeed(-4.5,-4)
         if potion_a_materials() == "magic_liquid_movement_faster" then
-            break
+            f:write(new_seed .. "\n")
         end
 
-        new_seed = new_seed + 1
-        if new_seed > 0x7FFFFFFF then --32b signed limit of seed
-            new_seed = 1
+        if GameGetFrameNum() ~= frame then
+            frame = GameGetFrameNum()
+            if 0 == (frame % 30) then
+                GamePrint("tested seed " .. new_seed)
+            end
         end
     end
+    f:close()
 end
